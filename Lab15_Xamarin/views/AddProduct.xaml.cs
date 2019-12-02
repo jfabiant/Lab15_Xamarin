8dc597eb4ffc055330d41e105d39ba15d3fcf5a4
John Fabian <john.timoteo@tecsup.edu.pe>
2019-12-02 07:27:53 -0500

    first commit

---
+++ Lab15_Xamarin/views/AddProduct.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net.Http;
+
+using Lab15_Xamarin.models;
+using Lab15_Xamarin.views;
+
+using Newtonsoft.Json;
+
+namespace Lab15_Xamarin.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddProduct : ContentPage
+    {
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAddProduct(object sender, EventArgs e)
+        {
+
+            // Encapsuling new product
+            ProductModel product = new ProductModel();
+            product.Nombre = "Producto Test";
+            product.Descripcion = "Descripcion Test";
+            product.Precio = "30";
+
+            HttpClient client = new HttpClient();
+            string url = "http://ec2-18-219-74-140.us-east-2.compute.amazonaws.com:5005/products/create";
+
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+
+            var results = await client.PostAsync(url, new StringContent(jsonProduct));
+
+            var json = results.Content.ReadAsStringAsync().Result;
+
+            System.Diagnostics.Debug.WriteLine("######## RESULTS ########");
+            System.Diagnostics.Debug.WriteLine(results);
+
+            await DisplayAlert("Informacion", json, "Ok");
+            
+        }
+    }
+}
---
+++ Lab15_Xamarin/views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Lab15_Xamarin.models;
+using Lab15_Xamarin.views;
+using Newtonsoft.Json;
+
+namespace Lab15_Xamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            requestWithMethodGet();
+        }
+
+        private async void requestWithMethodGet()
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://ec2-18-219-74-140.us-east-2.compute.amazonaws.com:5005/products/list";
+            var results = await client.GetAsync(url);
+            var json = results.Content.ReadAsStringAsync().Result;
+            //ProductModel model = ProductModel.FromJson(json);
+
+            var objects = JsonConvert.DeserializeObject<List<ProductModel>>(json);
+
+            listOfProducts.ItemsSource = objects;
+
+        }
+
+        private async void btnAddNewProduct(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddProduct());
+        }
+    }
+}
